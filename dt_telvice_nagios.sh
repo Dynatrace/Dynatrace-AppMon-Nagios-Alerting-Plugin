c4133e0dc5b308101f7d5001d4879e8d39f91f15
ingohackl <ingo.hackl@dynatrace.com>
2015-01-07 16:11:22 +0100

    version 1.0.0

--- dt_telvice_nagios.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-NAGIOS_SERVER=nagios-acc.khh.hu
-NAGIOS_PORT=5667
-NAGIOS_BIN=/opt/nsca/send_nsca
-NAGIOS_CFG=/opt/nsca/send_nsca.cfg
-
-echo $* | $NAGIOS_BIN -H $NAGIOS_SERVER -p $NAGIOS_PORT -c $NAGIOS_CFG -d '#'
